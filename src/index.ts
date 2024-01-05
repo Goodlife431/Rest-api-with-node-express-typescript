f3499c65bbe1283ac3b4ec9f8d5f9792b2f5bbfa
goodlife431 <oduwolejohn431@gmail.com>
2024-01-05 14:23:51 +0100

    mongodb

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
+import mongoose from 'mongoose';
+import { error } from 'console';
 
 const app = express();
 
@@ -16,3 +18,13 @@ app.use(bodyParser());
 app.use(bodyParser.json());
 
 const server = http.createServer(app);
+
+server.listen(8000, ()=> {
+    console.log("Server is running on http//:localhost:8000");
+})
+
+const MONGO_URL = 'mongodb+srv://oduwolejohn431:Mm1901318@cluster0.ynniptx.mongodb.net/?retryWrites=true&w=majority';
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on('error', (error: Error) => console.log(error));
